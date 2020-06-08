8b7d578142f0539d7e6d252133e538e90796eff4
Marius Dieckmann <mariusdieckmann90@gmail.com>
2020-06-08 13:04:38 +0200

    iterative improvements of API models

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-protoc --proto_path=. --go_out=plugins=grpc:go --go_opt=paths=source_relative proto/DatabaseModels.proto proto/LoadModels.proto proto/APIs.proto
+protoc --proto_path=. --go_out=plugins=grpc:go --go_opt=paths=source_relative proto/CommonModels.proto proto/DatabaseModels.proto proto/LoadModels.proto proto/APIs.proto
 mv go/proto/APIs.pb.go go/api
-mv go/proto/DatabaseModels.pb.go go/database
+mv go/proto/DatabaseModels.pb.go go/databasemodels
 mv go/proto/LoadModels.pb.go go/loadmodels
+mv go/proto/CommonModels.pb.go go/commonmodels
 
 rm -r go/proto
