6b68e2fa5bb7db869be62a1d10ac977bb1d18563
Marius Dieckmann <mariusdieckmann90@gmail.com>
2020-06-10 13:28:31 +0200

    added docstring

--- go/swagger/Swagger.go
+++ go/swagger/Swagger.go
@@ -2,6 +2,7 @@ package biodatadbapiswagger
 
 import "net/http"
 
+// GetSwaggerFS Returns FS access to the APIs swagger documentation
 func GetSwaggerFS() http.FileSystem {
 	fs := http.FileSystem(http.Dir("www/swagger-ui"))
 	return fs
