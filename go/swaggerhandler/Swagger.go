762264b75dc5d7940abbd89cc173c07613f8eece
Marius Dieckmann <mariusdieckmann90@gmail.com>
2020-06-10 16:23:11 +0200

    added debug message

--- go/swaggerhandler/Swagger.go
+++ go/swaggerhandler/Swagger.go
@@ -1,9 +1,14 @@
 package swaggerhandler
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 // GetSwaggerFS Returns FS access to the APIs swagger documentation
-func GetSwaggerFS() http.FileSystem {
+func GetSwaggerFS() *http.FileSystem {
+	log.Println(os.Getwd())
 	fs := http.FileSystem(http.Dir("www/swagger-ui"))
-	return fs
+	return &fs
 }
