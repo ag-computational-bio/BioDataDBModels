c04136dd5c1ab2f55b83de398527964212bcf532
Marius Dieckmann <mariusdieckmann90@gmail.com>
2020-06-14 17:40:00 +0200

    fixed dir struture for go files

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Temporary solution until ci/cd is enabled 
 protoc --proto_path=. --go_out=plugins=grpc:go --go_opt=paths=source_relative proto/CommonModels.proto proto/DatasetModels.proto proto/LoadModels.proto proto/APIs.proto
 mv go/proto/APIs.pb.go go/api
-mv go/proto/DatasetModels.pb.go go/databasemodels
+mv go/proto/DatasetModels.pb.go go/datasetmodels
 mv go/proto/LoadModels.pb.go go/loadmodels
 mv go/proto/CommonModels.pb.go go/commonmodels
 
