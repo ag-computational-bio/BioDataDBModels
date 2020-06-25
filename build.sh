320d7dcb21d6eece24daaf27f8a3c69cea618f2e
Marius Dieckmann <mariusdieckmann90@gmail.com>
2020-06-25 12:20:44 +0200

    added missing metadata models

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@
 
 # Builds the go stubs from the protobuf files and places them in their directories
 # Temporary solution until ci/cd is enabled 
-protoc --proto_path=. --go_out=plugins=grpc:go --go_opt=paths=source_relative proto/CommonModels.proto proto/DatasetModels.proto proto/LoadModels.proto proto/APIs.proto
+protoc --proto_path=. --go_out=plugins=grpc:go --go_opt=paths=source_relative proto/CommonModels.proto proto/DatasetModels.proto proto/LoadModels.proto proto/MetadataModels.proto proto/APIs.proto
 mv go/proto/APIs.pb.go go/api
 mv go/proto/DatasetModels.pb.go go/datasetmodels
 mv go/proto/LoadModels.pb.go go/loadmodels
 mv go/proto/CommonModels.pb.go go/commonmodels
+mv go/proto/MetadataModels.pb.go go/metadatamodels
+
 
 protoc -I. --grpc-gateway_out=logtostderr=true,paths=source_relative:./go/api proto/APIs.proto
 mv ./go/api/proto/APIs.pb.gw.go ./go/api
